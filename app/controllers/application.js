767c4e02b0d68908097e689befcdcb8f60f8eff3
Ryan LaBouve <imryanlabouve@gmail.com>
2018-02-13 23:02:47 -0600

    Attempt to recreate bug with simple object

---
+++ app/controllers/application.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  people: [
+    {
+      name: 'bob'
+    },
+    {
+      name: 'zim'
+    },
+    {
+      name: 'sally'
+    },
+  ]
+});
